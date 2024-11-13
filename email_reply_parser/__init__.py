689f312687e1a6d706df742b3aeb3200a22080fb
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-11-13 12:14:20 -0500

    remove mistaken `count=re.MULTILINE` argument

--- email_reply_parser/__init__.py
+++ email_reply_parser/__init__.py
@@ -65,7 +65,7 @@ class EmailMessage(object):
 
         # Fix any outlook style replies, with the reply immediately above the signature boundary line
         #   See email_2_2.txt for an example
-        self.text = re.sub('([^\n])(?=\n ?[_-]{7,})', '\\1\n', self.text, re.MULTILINE)
+        self.text = re.sub('([^\n])(?=\n ?[_-]{7,})', '\\1\n', self.text)
 
         self.lines = self.text.split('\n')
         self.lines.reverse()
